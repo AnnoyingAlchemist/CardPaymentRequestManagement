f685143ea7bd7de1bb564a7fa8f83b66174283d2
bpayero <payerob64@gmail.com>
2026-02-18 15:39:41 -0600

    OpenApi added, project structure changed.

--- demo/src/main/java/com/capgemini/demo/repository/CaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.demo.repository;
-
-import com.capgemini.demo.casefacade.CaseFacade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CaseRepository extends JpaRepository<CaseFacade, Long> {
-}
